7f31da1e4dcd1d9c12445f832c50af39ddf35e2f
savannahlut <savannahlut@nycstudents.net>
2024-11-26 20:03:16 +0000

    Finished least number

--- Algorithms.java
+++ Algorithms.java
@@ -9,8 +9,16 @@ public class Algorithms {
         f = new File("Numbers.txt");
         int odds = odds();
         int evens = evens();
-        System.out.println(odds);
-        System.out.println(evens);
+        int twoDigits = twoDigits();
+        int greater500 = greaterThan(500);
+        int greatest = greatest(); 
+        int lowest = least(); 
+        System.out.println("Odds: " + odds);
+        System.out.println("Evens: " + evens);
+        System.out.println("Double Digits: " + twoDigits);
+        System.out.println("Greater than 500: " + greater500);
+        System.out.println("The Greatest Value: " + greatest);
+        System.out.println("The Lowest Value: " + lowest);
         s.close();
     }
 
@@ -32,5 +40,45 @@ public class Algorithms {
                 evens++;
         }
         return evens;
+    }
+
+    public static int twoDigits() throws FileNotFoundException{
+        s = new Scanner(f);
+        int twoDigits = 0;
+        while (s.hasNext()) {
+            if (s.nextInt() > 9 && s.nextInt() < 100)
+                twoDigits++;
+        }
+        return twoDigits;
+    }
+
+    public static int greaterThan(int num) throws FileNotFoundException{
+        s = new Scanner(f);
+        int greater = 0;
+        while (s.hasNext()) {
+            if (s.nextInt() > num)
+                greater++;
+        }
+        return greater;
+    }
+
+    public static int greatest() throws FileNotFoundException{
+        s = new Scanner(f);
+        int greatest = Integer.MIN_VALUE;
+        while (s.hasNext()) {
+            if (s.nextInt() > greatest)
+            greatest = s.nextInt();
+        }
+        return greatest;
+    }
+
+    public static int least() throws FileNotFoundException{
+        s = new Scanner(f);
+        int least = Integer.MAX_VALUE;
+        while (s.hasNext()) {
+            if (s.nextInt() < least)
+                least = s.nextInt();
+        }
+        return least;
     }
 }
